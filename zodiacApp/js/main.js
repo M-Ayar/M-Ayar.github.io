00d0921981df3d116b26c281343a5847cb778a5f
M-Ayar <38121685+M-Ayar@users.noreply.github.com>
2019-02-24 21:00:20 -0800

    Add files via upload

---
+++ zodiacApp/js/main.js
@@ -0,0 +1,167 @@
+var zodiac = [
+	{
+        sign: "Aquarius",
+        personality: "Strengths include: being witty, clever, inventive and original.  Weakness: stubborn, unemotional, sarcastic, rebellious, aloof",
+        temperment: "Aquarius is the sign of visionaries, unconventionality and intellectual independence. Aquarius are the people who deviate from the crowd and go their own way. .",
+        img: "img/aquarius.jpg"
+    },
+    {
+        sign: "aquarius",
+        personality: "Strengths include: being witty, clever, inventive and original.  Weakness: stubborn, unemotional, sarcastic, rebellious, aloof",
+        temperment: "Aquarius is the sign of visionaries, unconventionality and intellectual independence. Aquarius are the people who deviate from the crowd and go their own way. .",
+        img: "img/aquarius.jpg",
+    },
+    {
+        sign: "Pisces",
+        personality: "Strengths: compassionate, adaptable, accepting,devoted and imaginative.   Weaknesses: Oversensitive, indecisive, self-pitying, lazy, escapist",
+        temperment: "Pisces is the sign of mysticism, mystery and the spiritual unknown. Pisces live in two worlds, the real world and the spiritual or mystical world where they interpret what they see into what they want. They do this to avoid all the realities of pain and suffering in the world. ",
+        img: "img/pisces.jpg",
+    },
+    {
+        sign: "pisces",
+        personality: "Strengths: compassionate, adaptable, accepting,devoted and imaginative.   Weaknesses: Oversensitive, indecisive, self-pitying, lazy, escapist",
+        temperment: "Pisces is the sign of mysticism, mystery and the spiritual unknown. Pisces live in two worlds, the real world and the spiritual or mystical world where they interpret what they see into what they want. They do this to avoid all the realities of pain and suffering in the world. ",
+        img: "img/pisces.jpg",
+    },
+    {
+        sign: "Aries",
+        personality: "Strengths: Independant, generous, optimistic, enthusiastic, courageous.   Weaknesses: moody, short tempered, self-involved, impulsive, inpatient",
+        temperment: "Aries is the first of the zodiac signs. Aries is the sign of the self, people born under this sign strongly project their personalities onto others and can be very self-oriented. ",
+        img: "img/aries.jpg",
+    },
+    {
+        sign: "aries",
+        personality: "Strengths: Independant, generous, optimistic, enthusiastic, courageous.   Weaknesses: moody, short tempered, self-involved, impulsive, inpatient",
+        temperment: "Aries is the first of the zodiac signs. Aries is the sign of the self, people born under this sign strongly project their personalities onto others and can be very self-oriented. ",
+        img: "img/aries.jpg",
+    },
+
+    {
+        sign: "Cancer",
+        personality: "Strengths: loyal, dependable, caring, adaptable, responsive.   Weaknesses: moody, clingy, self-pitying, Oversensitive, self-absorbed",
+        temperment: "Cancer is a mysterious sign, filled with contradictions. They want security and comfort yet seek new adventure. They are very helpful to others yet sometimes can be cranky and indifferent. Cancer has a driving, forceful personality that can be easily hidden beneath a calm, and cool exterior. The crab is Cancer's ruling animal and it suits them well, they can come out of their shell and fight but they can also hide in their shell of skitter away back into the depths of the ocean. They are very unpredictable. With cancer, there is always something more that meets the eye, for they are always partially hidden behind the shell. ",
+        img: "img/cancer.jpg",
+    },
+    {
+        sign: "cancer",
+        personality: "Strengths: loyal, dependable, caring, adaptable, responsive.   Weaknesses: moody, clingy, self-pitying, Oversensitive, self-absorbed",
+        temperment: "Cancer is a mysterious sign, filled with contradictions. They want security and comfort yet seek new adventure. They are very helpful to others yet sometimes can be cranky and indifferent. Cancer has a driving, forceful personality that can be easily hidden beneath a calm, and cool exterior. The crab is Cancer's ruling animal and it suits them well, they can come out of their shell and fight but they can also hide in their shell of skitter away back into the depths of the ocean. They are very unpredictable. With cancer, there is always something more that meets the eye, for they are always partially hidden behind the shell. ",
+        img: "img/cancer.jpg",
+    },
+    {
+        sign: "Capricorn",
+        personality: "Strengths: responsible, patient, amitious, resourceful, loyal.   Weaknesses: dictatorial, inhibited, conceited, distrusting, unimaginative",
+        temperment: "Capricorns are very ambitious people, they always have something they are pursuing and they want their lives to be fulfilled and important. ",
+        img: "img/capricorn.jpg",
+    },
+    {
+        sign: "capricorn",
+        personality: "Strengths: responsible, patient, amitious, resourceful, loyal.   Weaknesses: dictatorial, inhibited, conceited, distrusting, unimaginative",
+        temperment: "Capricorns are very ambitious people, they always have something they are pursuing and they want their lives to be fulfilled and important. ",
+        img: "img/capricorn.jpg",
+    },
+    {
+        sign: "Gemini",
+        personality: "Strengths: engergetic, clever, imaginative, witty, adaptable.   Weaknesses: superficial, impulsive, restless, devious, indecisive",
+        temperment: "Gemini people are many sided, quick both in the mind and physically. They are brimming with energy and vitality, they are clever with words. They are intelligent and very adaptable to every situation and every person. Gemini are curious and always want to know what's going on in the world around them.  ",
+        img: "img/gemini.jpg",
+    },
+    {
+        sign: "gemini",
+        personality: "Strengths: engergetic, clever, imaginative, witty, adaptable.   Weaknesses: superficial, impulsive, restless, devious, indecisive",
+        temperment: "Gemini people are many sided, quick both in the mind and physically. They are brimming with energy and vitality, they are clever with words. They are intelligent and very adaptable to every situation and every person. Gemini are curious and always want to know what's going on in the world around them.  ",
+        img: "img/gemini.jpg",
+    },
+    {
+        sign: "Leo",
+        personality: "Strengths: confident, ambitious, generous, loyal, encouraging.   Weaknesses: pretentious, domineering, melodramadic, stubborn, vain",
+        temperment: "Leo is the lion, this well suited symbol represents Leo very well. They possess a kingdom which they protest and cherish. The are high esteemed, honorable and very devoted to themselves in particular! The kingdom could be anything from work to home to a partner, whatever it is, you rule it. Leo is always center stage and full of flair, they enjoy basking in the spotlight. ",
+        img: "img/leo.jpg",
+    },
+    {
+        sign: "leo",
+        personality: "Strengths: confident, ambitious, generous, loyal, encouraging.   Weaknesses: pretentious, domineering, melodramadic, stubborn, vain",
+        temperment: "Leo is the lion, this well suited symbol represents Leo very well. They possess a kingdom which they protest and cherish. The are high esteemed, honorable and very devoted to themselves in particular! The kingdom could be anything from work to home to a partner, whatever it is, you rule it. Leo is always center stage and full of flair, they enjoy basking in the spotlight. ",
+        img: "img/leo.jpg",
+    },
+    {
+        sign: "Libra",
+        personality: "Strengths: diplomatic, graceful, peaceful, idealistic, hospitable.  Weaknesses: superficial, vain, indecisive, unreliable",
+        temperment: "Libras are the diplomat of the zodiac. They are able to put themselves in other's shoes and see things through another person's point of view. They are the ones that always want to make things right and have balance and harmony in their life, their surroundings and the lives of the people close to them. ",
+        img: "img/libra.jpg",
+    },
+    {
+        sign: "libra",
+        personality: "Strengths: diplomatic, graceful, peaceful, idealistic, hospitable.  Weaknesses: superficial, vain, indecisive, unreliable",
+        temperment: "Libras are the diplomat of the zodiac. They are able to put themselves in other's shoes and see things through another person's point of view. They are the ones that always want to make things right and have balance and harmony in their life, their surroundings and the lives of the people close to them. ",
+        img: "img/libra.jpg",
+    },
+    {
+        sign: "Sagittarius",
+        personality: "Strengths: Independence is Sagittarius' principle, they crave adventure and excitement and welcome change with open arms. Sagittarius is the sign of the philosopher and the explorer, they will go as far as road will go and explore every corner thoroughly in their ever eternal search for wisdom. ",
+        temperment: "Weakness:  unemotional, Sagittarius seem to be guided by luck, good things happen to Sagittarius and this is usually because of their optimistic outlook and positive disposition which attracts good fortune. ",
+        img: "img/sagittarius.jpg",
+    },
+    {
+        sign: "sagittarius",
+        personality: "Strengths: Independence is Sagittarius' principle, they crave adventure and excitement and welcome change with open arms. Sagittarius is the sign of the philosopher and the explorer, they will go as far as road will go and explore every corner thoroughly in their ever eternal search for wisdom. ",
+        temperment: "Weakness:  unemotional, Sagittarius seem to be guided by luck, good things happen to Sagittarius and this is usually because of their optimistic outlook and positive disposition which attracts good fortune. ",
+        img: "img/sagittarius.jpg",
+    },
+    {
+        sign: "Scorpio",
+        personality: "Strengths: loyal, passionate, resourceful, observant, dynamic Weaknesses: Jealous, Obsessive Manipulative",
+        temperment: "Scorpio is the astrology sign of extremes and intensity. Scorpios are very deep, intense people, there is always more then meets the eye. They present a cool, detached and unemotional air to the world yet lying underneath is tremendous power, extreme strength, intense passion and a strong will and a persistent drive. Scorpios have a very penetrative mind, do not be surprised if they ask questions, they are trying to delve deeper and figure things out and survey the situation.  ",
+        img: "img/scorpio.jpg",
+    },
+    {
+        sign: "scorpio",
+        personality: "Strengths: loyal, passionate, resourceful, observant, dynamic Weaknesses: Jealous, Obsessive Manipulative",
+        temperment: "Scorpio is the astrology sign of extremes and intensity. Scorpios are very deep, intense people, there is always more then meets the eye. They present a cool, detached and unemotional air to the world yet lying underneath is tremendous power, extreme strength, intense passion and a strong will and a persistent drive. Scorpios have a very penetrative mind, do not be surprised if they ask questions, they are trying to delve deeper and figure things out and survey the situation.  ",
+        img: "img/scorpio.jpg",
+    },
+    {
+        sign: "Taurus",
+        personality: "Strengths: dependable, persistant, loyal, patient, generous.  Weaknesses: stubborn, laziness, possessive, materialistic, self-indulging",
+        temperment: "Taurus is the one who has immense perseverance, even when others have given up, the Taurus rages on. Solid and persistent, just like the bull, which is Taurus' well suited symbol. Taurus's have a well known reputation for being stubborn, which is not necessarily a bad thing. The stubborn streak can cause Taurus to butt heads and conflict with other strong character types. Taurus are not fond of change. ",
+        img: "img/taurus.jpg",
+    },
+    {
+        sign: "taurus",
+        personality: "Strengths: dependable, persistant, loyal, patient, generous.  Weaknesses: stubborn, laziness, possessive, materialistic, self-indulging",
+        temperment: "Taurus is the one who has immense perseverance, even when others have given up, the Taurus rages on. Solid and persistent, just like the bull, which is Taurus' well suited symbol. Taurus's have a well known reputation for being stubborn, which is not necessarily a bad thing. The stubborn streak can cause Taurus to butt heads and conflict with other strong character types. Taurus are not fond of change. ",
+        img: "img/taurus.jpg",
+    },
+    {
+        sign: "Virgo",
+        personality: "Strengths: analytical, observant, helpful, reliable, precise.   Weaknesses: skeptical, fussy, inflexible, cold, interfering",
+        temperment: "Virgo exists in the mind, everything is inside. To the world, Virgo presents a calm and collected exterior but on the inside, nervous uncontrolled intensity in the mind, trying to figure things out, how to improve everything, analyzing and thinking.  ",
+        img: "img/virgo.jpg",
+    },
+    {
+        sign: "virgo",
+        personality: "Strengths: analytical, observant, helpful, reliable, precise.   Weaknesses: skeptical, fussy, inflexible, cold, interfering",
+        temperment: "Virgo exists in the mind, everything is inside. To the world, Virgo presents a calm and collected exterior but on the inside, nervous uncontrolled intensity in the mind, trying to figure things out, how to improve everything, analyzing and thinking.  ",
+        img: "img/virgo.jpg",
+    }
+]
+
+var input = document.getElementById("sign");
+var button = document.getElementById("button");
+var personality = document.getElementById("personality");
+var temperment = document.getElementById("temperment");
+var emptyImg = document.getElementById("emptyImg");
+var symbol = document.getElementById("sign");
+
+function getInfo() {
+	for (var i = 0; i < zodiac.length; i++){
+	if (input.value == zodiac[i].sign) {
+		symbol.innerText = zodiac[i].sign;
+		personality.innerText = zodiac[i].personality;
+		temperment.innerText = zodiac[i].temperment;				
+		emptyImg.src = zodiac[i].img;
+        emptyImg.alt = zodiac.sign;
+		}
+	}
+}
+
